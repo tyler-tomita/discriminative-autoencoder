1ffa4501e661b65c8cde8dddecec8bb421d18019
Tyler Tomita <tmtomita87@gmail.com>
2023-07-20 22:54:26 -0400

    add layer width_multiplier option

--- resnet_autoencoder.py
+++ resnet_autoencoder.py
@@ -19,25 +19,25 @@ class ResNetAutoencoder(nn.Module):
         num_classes: int = 1000
     ) -> None:
         super().__init__()
-        self.encoder = resnet18Encoder(encoder_block)
-        self.decoder = resnet18Decoder(reconstructed_shape, decoder_block)
+        self.encoder = resnet18Encoder(encoder_block, width_multiplier=width_multiplier)
+        self.decoder = resnet18Decoder(reconstructed_shape, decoder_block, width_multiplier=width_multiplier)
         self.projection_head = projection_head
         if self.projection_head == 'nonlinear':
             self.fc = nn.Sequential(
-                nn.Linear(512 * encoder_block.expansion, projection_head_size),
+                nn.Linear(int(512 * width_multiplier * encoder_block.expansion), projection_head_size),
                 nn.ReLU(),
                 nn.Linear(projection_head_size, num_classes)
             )
         elif self.projection_head == 'nonlinear2':
             self.fc = nn.Sequential(
-                nn.Linear(512 * encoder_block.expansion, projection_head_size),
+                nn.Linear(int(512 * width_multiplier * encoder_block.expansion), projection_head_size),
                 nn.ReLU(),
                 nn.Linear(projection_head_size, projection_head_size),
                 nn.ReLU(),
                 nn.Linear(projection_head_size, num_classes)
             )
         elif self.projection_head == 'linear':
-            self.fc = nn.Linear(512 * encoder_block.expansion, num_classes)
+            self.fc = nn.Linear(int(512 * width_multiplier * encoder_block.expansion), num_classes)
 
     def forward(self, x: Tensor, outputs: Optional[Tuple] = ('fc', 'decoder')) -> Tuple[Tensor, Tensor]:
         x = self.encoder(x)
